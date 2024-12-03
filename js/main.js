9a06d7caf10a0705faa62e257cb7e46dfb5a95e4
LVN <48881636+qLVN@users.noreply.github.com>
2024-12-03 22:58:36 +0100

    header dropdown navigation menu & more

--- js/main.js
+++ js/main.js
@@ -26,3 +26,73 @@ function openAnimatedLink(button, link) {
         window.open(link, '_top');
     }, 500); 
 }
+
+function openLink(page, where) {
+  if(where == "here") {
+      window.open(page, '_top');
+  }
+  else if(where == "newpage") {
+      window.open(page, '_blank');
+  }
+}
+
+displayedGroup = null;
+tabsOrder = ['publications', 'team'];
+
+function toggleHeaderDropdown(contentName) { //not the cleanest
+  dropdown = document.getElementById('header-dropdown');
+
+  if(contentName === null || contentName == displayedGroup) { //contentName = null means closing, contentName == displayedGroup means click on the opened tab, so closing.
+    //dropdown.style.opacity = 0;
+    dropdown.style.pointerEvents = 'none';
+    dropdown.classList.remove('fadein');
+
+    document.getElementById(displayedGroup + '-dropdown').style.opacity = 0;
+    document.getElementById(displayedGroup + '-dropdown').style.pointerEvents = 'none';
+    document.getElementById(displayedGroup + '-dropdown').classList.remove('animin-right');
+    document.getElementById(displayedGroup + '-dropdown').classList.remove('animin-left');
+
+    displayedGroup = null;
+
+    return;
+  }
+
+  fromRight = tabsOrder.indexOf(contentName) > tabsOrder.indexOf(displayedGroup); //new tab comes from the right
+
+  if(dropdown.style.pointerEvents != 'none') { //the dropdown was already opened on another tab, need to hide it then show the new one
+    //document.getElementById(displayedGroup + '-dropdown').style.opacity = 0;
+
+    document.getElementById(displayedGroup + '-dropdown').removeAttribute('style');
+    document.getElementById(displayedGroup + '-dropdown').style.pointerEvents = 'none';
+    document.getElementById(displayedGroup + '-dropdown').classList.remove('animin-right');
+    document.getElementById(displayedGroup + '-dropdown').classList.remove('animin-left');
+
+    if(fromRight) {
+      document.getElementById(displayedGroup + '-dropdown').classList.add('animout-left');
+    } else {
+      document.getElementById(displayedGroup + '-dropdown').classList.add('animout-right');
+    }
+  } else {
+    fromRight = null;
+  }
+
+  displayedGroup = contentName;
+
+  document.getElementById(displayedGroup + '-dropdown').classList.remove('animout-right');
+  document.getElementById(displayedGroup + '-dropdown').classList.remove('animout-left');
+
+  //dropdown.style.opacity = 1;
+  dropdown.classList.add('fadein');
+  dropdown.style.pointerEvents = 'all';
+    
+  // document.getElementById(contentName + '-dropdown').style.opacity = 1;
+  if(fromRight === null) {
+    document.getElementById(contentName + '-dropdown').style.opacity = 1
+  }
+  else if(fromRight) {
+    document.getElementById(contentName + '-dropdown').classList.add('animin-right');
+  } else {
+    document.getElementById(contentName + '-dropdown').classList.add('animin-left');
+  }
+  document.getElementById(contentName + '-dropdown').style.pointerEvents = 'all';
+}
