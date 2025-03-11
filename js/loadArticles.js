0efd825bbcae9b0f38d6e261751431b0af690167
LVN <48881636+qLVN@users.noreply.github.com>
2025-03-11 22:04:40 +0100

    post2

--- js/loadArticles.js
+++ js/loadArticles.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     pageName = window.location.pathname.split("/").pop().split(".")[0];
 
     try {
-        const response = await fetch('data/' + pageName + '.json');
+        const response = await fetch('https://cdn.lalliancegroup.com/assets/data/' + pageName + '.json?v=' + new Date().getTime());
         if (!response.ok) {
             throw new Error("ArticleLoadError");
         }
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             formatter = new Intl.DateTimeFormat('fr-FR', options);
 
             date = formatter.format(rawDate);
-            document.getElementById('content').innerHTML += '<div class="article-container"><div class="title">' + article['title'] + '</div><div class="content">' + article['body'] + '</div><div class="date">' + date + '</div><div class="author-wrapper"><img src="assets/material/authors/' + article['author'] + '.png" onerror="this.onerror=null;this.src=\'assets/material/authors/default.png\'" /><div>par ' + article['author'] + '</div></div></div>';
+            document.getElementById('content').innerHTML += '<div class="article-container small"><div class="title">' + article['title'] + '</div><div class="content">' + article['body'] + '</div><div class="date">' + date + '</div><div class="author-wrapper"><img src="https://cdn.lalliancegroup.com/assets/material/authors/' + article['author'] + '.png" onerror="this.onerror=null;this.src=\'https://cdn.lalliancegroup.com/assets/material/authors/default.png\'" /><div>par ' + article['author'] + '</div></div><div class="expand" onclick="this.style.display = \'none\'; this.parentElement.classList.remove(\'small\');">Voir en entier</div></div>';
         });
     } catch (error) {
         throw error;
